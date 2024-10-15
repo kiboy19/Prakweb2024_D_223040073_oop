4718145cb08f5ed2903fe2e70e088663d7f93896
Muhamad Ariel Septiadi <arielseptiadi3@gmail.com>
2024-10-15 23:46:40 +0700

    Update - Belajar Inherintance Bagian 2 Video ke-7

--- inheritance.php
+++ inheritance.php
@@ -8,11 +8,10 @@ class Produk
         $penerbit,
         $harga,
         $jmlHalaman,
-        $waktuMain,
-        $tipe;
+        $waktuMain;
 
     // Constructor Method
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe = "tipe")
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
     {
         $this->judul = $judul;
         $this->penulis = $penulis;
@@ -20,7 +19,6 @@ class Produk
         $this->harga = $harga;
         $this->jmlHalaman = $jmlHalaman;
         $this->waktuMain = $waktuMain;
-        $this->tipe = $tipe;
     }
 
     // Fungsi Mengembalikan Nilai dari Contructor
@@ -29,16 +27,28 @@ class Produk
         return "$this->penulis, $this->penerbit";
     }
 
-    public function getInfoLengkap()
+    public function getInfoProduk()
     {
         // Komik : Naruto | Masashi Khisimoto, Shonen Jump (Rp. 15000) - 100 Halaman.
-        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} {Rp. {$this->harga}}";
-        if ($this->tipe == "Komik") {
-            $str .= " - {$this->jmlHalaman} Halaman.";
-        } else if ($this->tipe == "Game") {
-            $str .= " ~ {$this->waktuMain} Jam.";
-        }
+        $str = "{$this->judul} | {$this->getLabel()} {Rp. {$this->harga}}";
+        return $str;
+    }
+}
 
+class Komik extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Komik: {$this->judul} | {$this->getLabel()} {Rp. {$this->harga}} - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str = "Game: {$this->judul} | {$this->getLabel()} {Rp. {$this->harga}} ~ {$this->waktuMain} Jam.";
         return $str;
     }
 }
@@ -53,12 +63,12 @@ class  CetakInfoProduk
 }
 
 
-$produk1 = new Produk("Naruto", "Masashi Kisimoto", "ShonenJump", 15000, 100, 0, "Komik");
-$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50, "Game");
+$produk1 = new Komik("Naruto", "Masashi Kisimoto", "ShonenJump", 15000, 100, 0);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50);
 
-echo $produk1->getInfoLengkap();
+echo $produk1->getInfoProduk();
 echo "<br>";
-echo $produk2->getInfoLengkap();
+echo $produk2->getInfoProduk();
 
 
 
