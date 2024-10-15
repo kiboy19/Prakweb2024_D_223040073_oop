233d531c22b39615a29456575f6fbe094b12a0cc
Muhamad Ariel Septiadi <arielseptiadi3@gmail.com>
2024-10-15 23:01:30 +0700

    Update - Belajar Inherintance Bagian 1 Video ke-6

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -6,21 +6,40 @@ class Produk
     public $judul,
         $penulis,
         $penerbit,
-        $harga;
+        $harga,
+        $jmlHalaman,
+        $waktuMain,
+        $tipe;
 
     // Constructor Method
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe = "tipe")
     {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
     }
 
     // Fungsi Mengembalikan Nilai dari Contructor
     public function getLabel()
     {
         return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoLengkap()
+    {
+        // Komik : Naruto | Masashi Khisimoto, Shonen Jump (Rp. 15000) - 100 Halaman.
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} {Rp. {$this->harga}}";
+        if ($this->tipe == "Komik") {
+            $str .= " - {$this->jmlHalaman} Halaman.";
+        } else if ($this->tipe == "Game") {
+            $str .= " ~ {$this->waktuMain} Jam.";
+        }
+
+        return $str;
     }
 }
 
@@ -34,8 +53,12 @@ class  CetakInfoProduk
 }
 
 
-$produk1 = new Produk("Naruto", "Masashi Kisimoto", "ShonenJump", 15000);
-$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000);
+$produk1 = new Produk("Naruto", "Masashi Kisimoto", "ShonenJump", 15000, 100, 0, "Komik");
+$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50, "Game");
+
+echo $produk1->getInfoLengkap();
+echo "<br>";
+echo $produk2->getInfoLengkap();
 
 // echo "Komik: " . $produk1->getLabel();
 // echo "<hr>";
