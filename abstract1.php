eb2f66f63f84373eae5b766414efb658f827db69
Muhamad Ariel Septiadi <arielseptiadi3@gmail.com>
2024-10-16 02:27:14 +0700

    Update - Belajar Abstrak Bagian 1 - Video ke-13

---
+++ abstract1.php
@@ -0,0 +1,13 @@
+<?php
+
+abstract class Buah
+{
+    private $warna;
+
+    abstract public function makan();
+
+    public function setWarna($warna)
+    {
+        $this->warna = $warna;
+    }
+}
